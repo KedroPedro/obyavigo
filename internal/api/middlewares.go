af84345711249b571333219b09bd21e1df4d050f
KedroPedro <zyazyulyam@bk.ru>
2025-11-03 16:23:09 +0300

    a

--- internal/api/middlewares.go
+++ internal/api/middlewares.go
@@ -38,7 +38,6 @@ func (h *Handlers) AuthMiddleware(next http.Handler) http.Handler {
 				return
 			}
 
-			// check if user is banned; if so, clear cookie and proceed as guest
 			status, err := h.db.Psql.GetUserStatus(id)
 			if err == nil && status == "banned" {
 				http.SetCookie(w, &http.Cookie{
